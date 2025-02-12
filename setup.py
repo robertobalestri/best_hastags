d21a38873236829672a94d794ce29d3590491542
Rob Bal <robebal>
2025-02-12 11:46:37 +0100

    modified:   README.md  	new file:   best_hashtags.egg-info/PKG-INFO  	new file:   best_hashtags.egg-info/SOURCES.txt  	new file:   best_hashtags.egg-info/dependency_links.txt  	new file:   best_hashtags.egg-info/requires.txt  	new file:   best_hashtags.egg-info/top_level.txt  	new file:   build/lib/src/__init__.py  	new file:   build/lib/src/best_hashtags.py  	deleted:    dist/best_hashtags-0.1.0-py3-none-any.whl  	deleted:    dist/best_hashtags-0.1.0.tar.gz  	new file:   dist/best_hashtags-0.1.1-py3-none-any.whl  	new file:   dist/best_hashtags-0.1.1.tar.gz  	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='best_hashtags',
-    version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'requests',
-        'beautifulsoup4',
-    ],
-    entry_points={
-        'console_scripts': [
-            'best_hashtags=best_hashtags:main',
-        ],
-    },
-    author='Roberto Balestri',
-    author_email='roberto.balestri2@unibo.it',
-    description='A simple hashtag scraper',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/robertobalestri/best_hashtags',
+    name="best_hashtags",
+    version="0.1.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A tool to find the best hashtags for a given word",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robertobalestri/best_hashtags",
+    packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ],
 )
